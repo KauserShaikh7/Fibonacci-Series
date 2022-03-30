cf7ef860e395fb6cc7a813c25c061eecfcef75d7
hp <hp@192.168.0.12>
2022-03-30 16:24:28 +0530

    yes Commit
    Uploading Project

---
+++ Towards.Java/src/JavaChallenges/WithoutRecurtion.java
@@ -0,0 +1,22 @@
+package JavaChallenges;
+
+public class WithoutRecurtion {
+
+	public static void main(String[] args) {
+		int x1=0,x2=1,x3;
+		int count=10;
+		 System.out.println("Fibonacci series: ");
+		 System.out.println(x1+""+x2);
+		 for(int i=2;i<count;i++) 
+		 {
+		 x3=x1+x2;
+		 System.out.println(" "+x3);
+		 x1=x2;
+         x2=x3;
+		 }
+
+
+	}
+
+}
+//without recurtion
